53d15638ce0549d67283267c19e3a54d1ebf074d
Veronica Lang <TheSlug64@gmail.com>
2017-10-04 09:16:45 -0400

    Create Tester.java

---
+++ src/com/company/Tester.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by Veronica Lang on 10/4/2017.
+ */
+public class Tester {
+    public static void main(String arg[]) {
+        Circle cir1 = new Circle(35.5);
+        System.out.println(cir1.diameter());
+    }
+}
